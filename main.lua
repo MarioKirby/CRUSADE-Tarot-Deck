60686e007cd52339304f4a4f9fca98e0243b925d
Mario Kirby <37307714+MarioKirby@users.noreply.github.com>
2026-06-21 18:57:34 -0700

    Add files via upload

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@
 --- MOD_ID: crusade_tarot
 --- PREFIX: crusade
 --- MOD_AUTHOR: [Luigi Kirby]
---- MOD_DESCRIPTION: Tarot cards based on LK's characters
+--- MOD_DESCRIPTION: Tarot and Loteria cards based on LK's characters
 --- VERSION: 1.0.0
 --- DEPENDENCIES: [malverk]
 
@@ -29,3 +29,25 @@ TexturePack{
         }
     }
 }
+
+if next(SMODS.find_mod('ortalab')) then
+    AltTextures_Utils.default_atlas['ortalab_loteria'] = 'ortalab_loteria_cards'
+    AltTextures_Utils.loc_keys['ortalab_loteria'] = 'b_ortalab_loteria_cards'
+    AltTexture({
+        key = 'alt_loteria',
+        set = 'ortalab_loteria',
+        path = 'woahswap_loteria.png',
+        loc_txt = { name = 'WOAHSwap Loteria' }
+    })
+
+    TexturePack({
+        key = 'woahswap',
+        textures = {
+            'crusade_alt_loteria'
+        },
+        loc_txt = {
+            name = 'WOAHSwap Loteria',
+            text = { 'WOAHSwap AU themed', '{C:attention}Loteria Cards!{}!', '{C:inactive}(When disabling,{}', '{C:inactive}restart the game){}' }
+        }
+    })
+end 
